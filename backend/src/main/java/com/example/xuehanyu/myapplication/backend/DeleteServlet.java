cc52204f2c426edee744f36bbdb85268b9f9fbfe
dnalwqer <liuweiqiang1992@gmail.com>
2016-05-16 20:19:44 -0400

    Backend

---
+++ backend/src/main/java/com/example/xuehanyu/myapplication/backend/DeleteServlet.java
@@ -0,0 +1,33 @@
+package com.example.xuehanyu.myapplication.backend;
+
+import com.example.xuehanyu.myapplication.backend.data.EntryDataStore;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by dnalwqer on 5/16/16.
+ */
+public class DeleteServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        String id = req.getParameter("ID");
+        EntryDataStore.delete(id);
+        resp.sendRedirect("/query.do");
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        doGet(req, resp);
+    }
+}
